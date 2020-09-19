6045da34e2edd2f17e3375db6ff4c856c3622542
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-09-19 12:13:04 -0400

    Se agrego el UnitTest

---
+++ ParcialNicolasOsinaga/UnitTestCountry/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCountry")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCountry")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10800dc4-596d-4e34-933b-0db05e0a8607")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ParcialNicolasOsinaga/UnitTestCountry/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ApiCountry.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCountry
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodGet()
+        {
+            //Arrange
+            CountriesController countriesController = new CountriesController();
+            //Act
+            var listapais = countriesController.GetCountries();
+            //Assert
+            Assert.IsNotNull(listapais);
+        }
+    }
+}
