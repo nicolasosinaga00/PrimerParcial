f73335c0a7c36817f03ee299afcb7b7c212e2f59
nicolasosinaga00 <a2019114771@estudiantes.upsa.edu.bo>
2020-09-19 11:43:24 -0400

    Se creo el modelo y se cambio el webconfig

---
+++ ParcialNicolasOsinaga/ApiCountry/Models/Country.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiCountry.Models
+{
+    public enum regiontype
+    {
+        America = 1,
+        Europa = 2,
+        Africa = 3,
+        Asia = 4,
+        Oceania = 5
+
+    };
+    public class Country
+    {
+        [Key]
+        [Required(ErrorMessage ="You must enter the field {0}")]
+        [StringLength(3, ErrorMessage ="The field {0} must contain between {2} and {1} characters", MinimumLength =3)]
+        public string alpha3code { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public regiontype region { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public double area { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public int callingcodes { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string languages { get; set; }
+
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        [Url]
+        public string flag { get; set; }
+
+    }
+}
